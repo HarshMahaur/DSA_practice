1f20c64cf4934de6d34186589cafda5eb0f4d90e
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-07-06 20:17:26 +0530

    min heap O(N)
    it passes whole array at once instead of one by one so O(N) instead ofO(NlogN)

--- PriorityQueue/inbuilt_min_pq.cpp
+++ PriorityQueue/inbuilt_min_pq.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main(){
     
     priority_queue<int,vector<int>,greater<int>> pq; //  this is max heap 
+    //priority_queue<int,vector<int>,greater<int>> pq(a,a+n); // O(N) - array
+    //priority_queue<int,vector<int>,greater<int>> pq(v.begoin(),v.end()); // O(N)- vector
+    
     pq.push(68);
     pq.push(67);
     pq.push(9);
