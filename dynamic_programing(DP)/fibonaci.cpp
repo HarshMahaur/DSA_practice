28b08d9b39ed8827c26f6b2a5be5a827239e46c5
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-07-07 19:49:59 +0530

    fibo_dp_another type
    fdirect approach - filliting the array while initializing to number with correct value

--- dynamic_programing(DP)/fibonaci.cpp
+++ dynamic_programing(DP)/fibonaci.cpp
@@ -7,11 +7,11 @@ int fibo(int n){
     return fibo(n-1)+fibo(n-2);
 }
 
-int fibo2(int n,int *arr){
+int fibo2(int n,vector<int>& arr){
     if(n==0 || n==1){
         return n;
     }
-    if(arr[n]!=0){
+    if(arr[n]!=-1){
         return arr[n];
     }
     int output= fibo2(n-1,arr)+ fibo2(n-2,arr);
@@ -19,12 +19,26 @@ int fibo2(int n,int *arr){
     return output;
 }
 
+int fibo3(int n){
+    int *arr = new int[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<n+1;i++){
+        arr[i]= arr[i-1]+arr[i-2];
+    }
+    int output = arr[n];
+    delete [] arr;
+    return output;
+
+}
+
 
 int main(){
     int n;
     std::cin>>n ;
-    int arr[n];
+    vector<int> arr(n+1,-1);
     cout<< fibo2(n,arr);
+    // cout<< fibo3(n);
 
 
 
