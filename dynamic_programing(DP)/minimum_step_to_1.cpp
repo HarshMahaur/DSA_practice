e24d582ed5fcd058b2bc650a80c6cb7b7e7c5b86
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-07-08 19:38:47 +0530

    memorization_method_minsteapsto1
    used array to store the results and reuse

--- dynamic_programing(DP)/minimum_step_to_1.cpp
+++ dynamic_programing(DP)/minimum_step_to_1.cpp
@@ -1,6 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// memorization solution
+int helper(int n,int *ans){
+    if(n==1){
+        return 0;
+    }
+    if(ans[n]!=-1){
+        return ans[n];
+    }
+    int x= helper(n-1,ans);
+    int y{INT_MAX};
+    int z{INT_MAX};
+    if(n%2==0){
+        y= helper(n/2,ans);
+    }
+    if(n%3==0){
+        z= helper(n/3,ans);
+    }
+    int output = min({x, y, z})+1;
+    ans[n]=output;
+    return output;
+
+
+}
+int minStepto1Mem(int n ){
+    int *ans=new int[n+1];
+    for(int i=0;i<n+1;i++){
+        ans[i]=-1;
+    }
+    return helper(n,ans);
+}
+
+
 //solution by me for - minStepTo1
 
 int minStepTo1(int n){
@@ -24,6 +56,7 @@ int minStepTo1(int n){
 
 
 int main(){
-    std::cout<< minStepTo1(7) <<std::endl;
+    // std::cout<< minStepTo1(1000) <<std::endl;
+    std::cout<< minStepto1Mem(1000) <<std::endl;
     return 0;
 }
