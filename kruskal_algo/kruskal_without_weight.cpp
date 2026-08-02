59f5c370b1c87ad1a02f8cbf0651179ed281b4e0
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-08-02 13:31:29 +0530

    kruskal algo worked
    the code is for non weighted graph and the code was soly written by me

--- kruskal_algo/kruskal_without_weight.cpp
+++ kruskal_algo/kruskal_without_weight.cpp
@@ -1,70 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-// this guntion kinda useless- hashmap for visited would make more sence as visted array suppose that the graph valud are under the n and no repeated values.
-void printDFS(vector<vector<int>> matrix,vector<bool>& visited,int sv){
-    
-    std::cout << sv<<std::endl;
-    visited[sv]=true;
-    
-    int n = matrix.size();
-    for(int i =0;i<n;i++){
-        if(matrix[sv][i]==1 && visited[i]==false){
-            printDFS(matrix,visited,i);
-        }
-        
-    }
-}
 
-// mine code attempt
-void printBFS(vector<vector<int>> matrix,vector<bool>& visited,queue<int> q,int sv){
-    int n = matrix.size();
-    q.push(sv);
-    visited[sv]=true;
-    while(!q.empty()){
-        sv= q.front();
-        std::cout<< sv <<std::endl;
-        q.pop();
-        
-        for(int i =0;i<n;i++){
-            if(matrix[sv][i]==1 && visited[i]==false){
-                visited[i]=true;
-                q.push(i);
-            }
-        }
-    }
-    
-}
-void DFS(vector<vector<int>> matrix){
-    int n= matrix.size();
-    vector<bool> visited(n,false);
-    int components{0};
-    for(int i =0;i<n;i++){
-        if(!visited[i]){
-            printDFS(matrix,visited,i);
-            components++;
-            
-        }
-    }
-    std::cout<< "no of components by BFS : " << components<<std::endl;
-}
-
-void BFS(vector<vector<int>> matrix){
-    int n= matrix.size();
-    vector<bool> visited(n,false);
-    queue<int> que;
-    int components{0};
-    for(int i =0;i<n;i++){
-        if(!visited[i]){
-            printBFS(matrix,visited,que,i);
-            components++;
-        }
-
-    }
-    std::cout<< "no of components by BFS : " << components<<std::endl;
-    
-}
 
 // mine code attempt to kruskal
+// and this shi actually worked.
 
 void makeSet(vector<int>& parent, int i){
     parent[i]=i;
@@ -90,6 +29,10 @@ vector<pair<int,int>> MST_Kruskal(vector<vector<int>> grap){
     // unordered_set<int> visited;
 
     // to make the indivisual set;
+    // i also need a vector of vector/or set that contains the edges only, so i do not have to loop over the whole matrix again and again;
+    // i will create a unsorted set of pair that contains edges;
+    set<pair<int,int>> edges;
+    
     for(int i = 0;i<n;i++){
         for(int j=0;j<m;j++){
             if(grap[i][j]==1){
@@ -103,11 +46,25 @@ vector<pair<int,int>> MST_Kruskal(vector<vector<int>> grap){
                 if(parent[j]==-1){
                     makeSet(parent,j);
                 }
+                if(edges.count({i,j})==0 && edges.count({j,i})==0){
+                    edges.insert({i,j});
+                }
             }
         }
     }
 
-    //
+    //for now there will be no traversing on the basis of weight;
+    vector<pair<int,int>> MST;
+    for(auto c: edges){
+        int a = c.first;
+        int b = c.second;
+        if(find(parent,a)!=find(parent,b)){
+            Union(parent,a,b);
+            MST.push_back(c);
+        }
+    }
+    return MST;
+
     
 
 
@@ -134,12 +91,19 @@ int main(){
 
     }
 
-    std::cout<< "DFS output : " <<std::endl;
-    DFS(grap);
+    // std::cout<< "DFS output : " <<std::endl;
+    // DFS(grap);
 
     
     // std::cout<< "BFS output : " <<std::endl;
     // BFS(grap);
+
+    std::cout<< "output fo the minimum spanning tree is : " <<std::endl;
+    vector<pair<int,int>> MST;
+    MST= MST_Kruskal(grap);
+    for(auto c: MST){
+        std::cout<< c.first << " " << c.second <<std::endl;
+    }
 
 
 
@@ -202,4 +166,125 @@ input 3 -
 0 4
 1 4
 2 5
+*/
+
+/*
+
+input for the kurskal:
+9
+13
+0 1
+0 7
+1 2
+7 8
+7 6
+7 1
+8 2
+8 6
+6 5
+5 2
+5 3
+2 3
+5 4
+3 4
+
+input 2 - 
+7
+11
+0 1
+0 2
+0 3
+1 2
+1 4
+2 5
+3 4
+3 6
+4 5
+4 6
+5 6
+
+big ahh input-
+30
+80
+0 1
+0 2
+0 5
+0 10
+1 2
+1 3
+1 6
+1 11
+2 3
+2 4
+2 7
+2 12
+3 4
+3 8
+3 13
+4 9
+4 14
+5 6
+5 10
+5 15
+6 7
+6 11
+6 16
+7 8
+7 12
+7 17
+8 9
+8 13
+8 18
+9 14
+9 19
+10 11
+10 15
+10 20
+11 12
+11 16
+11 21
+12 13
+12 17
+12 22
+13 14
+13 18
+13 23
+14 19
+14 24
+15 16
+15 20
+15 25
+16 17
+16 21
+16 26
+17 18
+17 22
+17 27
+18 19
+18 23
+18 28
+19 24
+19 29
+20 21
+21 22
+22 23
+23 24
+24 29
+25 26
+26 27
+27 28
+28 29
+0 29
+2 28
+4 26
+6 24
+8 22
+10 20
+1 21
+3 23
+5 25
+7 27
+9 29
+11 29 
+
 */
