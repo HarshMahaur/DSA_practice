f326ec062667f1ea5a061cfa2ca661e3f18ad593
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-08-17 20:52:46 +0530

    Create trie_implimentation.cpp

---
+++ trie/trie_implimentation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    vector<string> dict;
+    dict.push_back("ARE");
+    dict.push_back("AS");
+    dict.push_back("DO");
+    dict.push_back("DOT");
+    dict.push_back("NEW");
+    dict.push_back("NEWS");
+    dict.push_back("NO");
+    dict.push_back("NOT");
+
+
+
+
+
+    return 0;
+}
