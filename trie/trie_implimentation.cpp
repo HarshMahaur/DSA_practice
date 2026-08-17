ed4fb7e57a75b62360734e39cdc799a0c5b808a1
HarshMahaur <229552607+HarshMahaur@users.noreply.github.com>
2026-08-17 21:39:04 +0530

    add funtion for trie is added

--- trie/trie_implimentation.cpp
+++ trie/trie_implimentation.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    bool isTerminal;
+    Node* child[26];
+
+    node(){
+        for(int i=0;i<26;i++){
+            child[i]=NULL;
+        }
+    }
+};
+
+void add(string w,Node* trie){
+    int n= w.size();
+    for(int i =0;i<n;i++){
+        if(trie->child[w[i]-'A']=NULL){
+            trie->child[w[i]-'A']=new Node();
+        }
+        trie=trie->child[w[i]-'A'];
+    }
+    trie->isTerminal=true;
+
+}
+
 
 int main(){
     vector<string> dict;
@@ -12,6 +36,14 @@ int main(){
     dict.push_back("NEWS");
     dict.push_back("NO");
     dict.push_back("NOT");
+
+
+    Node* root= new Node();
+
+    for (int i =0;i<dict.size();i++){
+        add(dict[i],root);
+
+    }
 
 
 
